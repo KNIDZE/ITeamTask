7e0fc3f07f3087fa223d4140bf14348f5168f825
KNIDZE <mihaknidze@gmail.com>
2024-03-22 23:08:25 +0200

    HomeLoaderAdded

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import Loader from '@/components/loader';
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [shouldFetch, setShouldFetch] = useState(false);
-  const { data } = useSWR(shouldFetch ? searchTerm : null, listJobsFetcher);
+  const { data, isLoading } = useSWR(shouldFetch ? searchTerm : null, listJobsFetcher);
   const handleSearch = () => {
     setShouldFetch(true);
   };
@@ -41,6 +41,7 @@ const Home = () => {
             </button>
           </div>
           {data && <JobsGrid jobs={data} />}
+          {isLoading && <Loader />}
         </div>
       </div>
     </Suspense>
